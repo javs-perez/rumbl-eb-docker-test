cd3267d1995d0966d632962ca6ad14bc4f3dcee1
Javier Perez <perez.javi.a@gmail.com>
2016-10-12 16:29:34 -0400

    adding deployment to aws eb

---
+++ setup-eb.sh
@@ -0,0 +1,11 @@
+set -x
+set -e
+
+mkdir $HOME/.aws
+touch $HOME/.aws/config
+chmod 600 $HOME/.aws/config
+echo "[profile eb-cli]" > $HOME/.aws/config
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> $HOME/.aws/config
+echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $HOME/.aws/config
+echo "[default]" > $HOME/.aws/config
+echo "region = us-east-1" > $HOME/.aws/config
