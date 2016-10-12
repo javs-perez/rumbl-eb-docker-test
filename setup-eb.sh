ba17988720f6ab98eb240281e1385d8f31ccb274
Javier Perez <perez.javi.a@gmail.com>
2016-10-12 17:18:10 -0400

    add >> instead of >

--- setup-eb.sh
+++ setup-eb.sh
@@ -7,5 +7,5 @@ chmod 600 /home/ubuntu/.aws/config
 echo "[profile eb-cli]" > /home/ubuntu/.aws/config
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /home/ubuntu/.aws/config
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/.aws/config
-echo "[default]" > /home/ubuntu/.aws/config
-echo "region = us-east-1" > /home/ubuntu/.aws/config
+echo "[default]" >> /home/ubuntu/.aws/config
+echo "region = us-east-1" >> /home/ubuntu/.aws/config
